bfae796d45420a4572090c97e4df941349a4ca68
Mikhail Libchenko <libchenkomikhail@gmail.com>
2022-05-19 09:11:42 +0300

    fix: FileGenerator updated with string builder

--- lab-4/PerfTips.Shared/Serializer/Utf8Serializer.cs
+++ lab-4/PerfTips.Shared/Serializer/Utf8Serializer.cs
@@ -1,18 +1,8 @@
-using Microsoft.IO;
-
 namespace PerfTips.Shared.Serializer;
 
 public class Utf8Serializer : ISerializer
 {
-    private static readonly RecyclableMemoryStreamManager Manager = new();
+    public byte[] Serialize<T>(T obj) => Utf8Json.JsonSerializer.Serialize(obj);
 
-    public byte[] Serialize<T>(T obj)
-    {
-        return Utf8Json.JsonSerializer.Serialize(obj);
-    }
-
-    public T Deserialize<T>(byte[] bytes)
-    {
-        return Utf8Json.JsonSerializer.Deserialize<T>(bytes);
-    }
+    public T Deserialize<T>(byte[] bytes) => Utf8Json.JsonSerializer.Deserialize<T>(bytes);
 }
