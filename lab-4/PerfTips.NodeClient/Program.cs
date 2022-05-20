eec1090780ac77b30e678bdd20260ea098504445
Mikhail Libchenko <libchenkomikhail@gmail.com>
2022-05-20 20:26:06 +0300

    fix: minor fixes

--- lab-4/PerfTips.NodeClient/Program.cs
+++ lab-4/PerfTips.NodeClient/Program.cs
@@ -3,58 +3,56 @@ using System.Net.Sockets;
 using AutoMapper;
 using PerfTips.NodeClient.TcpNode;
 using PerfTips.Shared.PackageManager;
+using PerfTips.NodeClient;
 
-namespace PerfTips.NodeClient;
+var appSettings = Startup.AppSettings;
+string server = appSettings.Server;
 
-public static class Program
+if (args.Any() && int.TryParse(args[0], out int port))
+    port = int.Parse(args[0]);
+else
 {
-    public static async Task Main(string[] args)
+    Console.Write("Port: ");
+    port = int.Parse(Console.ReadLine()!);
+}
+
+var tcpEndPoint = new IPEndPoint(IPAddress.Parse(server), port);
+var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+socket.Bind(tcpEndPoint);
+Console.WriteLine($"Endpoint started listening at: {server}:{port}");
+socket.Listen();
+
+IPackageManager packageManager = Startup.PackageManager;
+IMapper mapper = Startup.Mapper;
+
+TcpNodeInstance nodeInstance = new(Path.Combine(Startup.DesktopPath, appSettings.RelativePath), IPAddress.Parse(server),
+    port, mapper, packageManager);
+
+var cts = new CancellationTokenSource();
+
+Console.CancelKeyPress += (s, e) =>
+{
+    cts.Cancel();
+    e.Cancel = true;
+};
+
+try
+{
+    while (!cts.IsCancellationRequested)
     {
-        var appSettings = Startup.AppSettings;
-        string server = appSettings.Server;
+        using var listener = await socket.AcceptAsync(cts.Token);
 
-        // Console.Write("Enter port: ");
-        var port = int.Parse(args[0]);
-
-        var tcpEndPoint = new IPEndPoint(IPAddress.Parse(server), port);
-        var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-        socket.Bind(tcpEndPoint);
-        Console.WriteLine($"Endpoint started listening at: {server}:{port}");
-        socket.Listen();
-
-        IPackageManager packageManager = Startup.PackageManager;
-        IMapper mapper = Startup.Mapper;
-
-        TcpNodeInstance nodeInstance = new (Path.Combine(Startup.DesktopPath, appSettings.RelativePath), IPAddress.Parse(server), port, mapper, packageManager);
-
-        var cts = new CancellationTokenSource();
-
-        Console.CancelKeyPress += (s, e) =>
-        {
-            cts.Cancel();
-            e.Cancel = true;
-        };
-
-        try
-        {
-            while (!cts.IsCancellationRequested)
-            {
-                /* Creating listener for our socket  */
-                using var listener = await socket.AcceptAsync(cts.Token);
-
-                await nodeInstance.Execute(listener, cts);
-            }
-
-            cts.Token.ThrowIfCancellationRequested();
-        }
-        catch (OperationCanceledException)
-        {
-            Console.WriteLine("Cancellation requested, node stopped");
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-        }
+        await nodeInstance.Execute(listener, cts);
     }
+
+    cts.Token.ThrowIfCancellationRequested();
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Cancellation requested, node stopped");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
 }
--- lab-4/PerfTips.ServerClient/Program.cs
+++ lab-4/PerfTips.ServerClient/Program.cs
@@ -3,51 +3,44 @@ using AutoMapper;
 using PerfTips.ServerClient.DataProviders;
 using PerfTips.ServerClient.TcpServer;
 using PerfTips.Shared.PackageManager;
+using PerfTips.ServerClient;
 
-namespace PerfTips.ServerClient;
+if (args.Any() && bool.TryParse(args[0], out bool isWithFilesGeneration) && isWithFilesGeneration)
+    PerfTips.FilesGenerator.Program.Main();
 
-public static class Program
+var appSettings = Startup.AppSettings;
+
+IDataProvider commandsProvider = Startup.DataProvider;
+IPackageManager packageManager = Startup.PackageManager;
+IMapper mapper = Startup.Mapper;
+
+var cts = new CancellationTokenSource();
+
+Console.CancelKeyPress += (s, e) =>
 {
-    public static async Task Main(string[] args)
+    cts.Cancel();
+    e.Cancel = true;
+};
+
+try
+{
+    Server server = new(IPAddress.Parse(appSettings.Server), appSettings.Port, mapper, commandsProvider, packageManager);
+
+    while (!cts.IsCancellationRequested)
     {
-        if (args.Any() && bool.TryParse(args[0], out bool isWithFilesGeneration) && isWithFilesGeneration)
-            FilesGenerator.Program.Main();
-
-        var appSettings = Startup.AppSettings;
-
-        IDataProvider commandsProvider = Startup.DataProvider;
-        IPackageManager packageManager = Startup.PackageManager;
-        IMapper mapper = Startup.Mapper;
-
-        var cts = new CancellationTokenSource();
-        
-        Console.CancelKeyPress += (s, e) =>
-        {
-            cts.Cancel();
-            e.Cancel = true;
-        };
-
-        try
-        {
-            Server server = new (IPAddress.Parse(appSettings.Server), appSettings.Port, mapper, commandsProvider, packageManager);
-
-            while (!cts.IsCancellationRequested)
-            {
-                var command = commandsProvider.GetCommand();
-                await server.Execute(command, cts);
-            }
-
-            cts.Token.ThrowIfCancellationRequested();
-        }
-        catch (OperationCanceledException)
-        {
-            Console.WriteLine("Cancellation requested, program stopped");
-            Console.ReadLine();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            Console.ReadLine();
-        }
+        var command = commandsProvider.GetCommand();
+        await server.Execute(command, cts);
     }
+
+    cts.Token.ThrowIfCancellationRequested();
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Cancellation requested, program stopped");
+    Console.ReadLine();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    Console.ReadLine();
 }
